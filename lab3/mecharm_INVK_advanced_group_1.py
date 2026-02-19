190972a9d2e554896483cc9be95f04d5ddf99323
abant07 <abantwal2007@gmail.com>
2026-02-19 14:45:47 -0800

    advanced ik

--- lab3/mecharm_INVK_advanced_group_1.py
+++ lab3/mecharm_INVK_advanced_group_1.py
@@ -1,62 +1,79 @@
 import numpy as np
-from sympy  import symbols, cos, sin, atan2, pi, Matrix, lambdify 
+from sympy  import symbols, cos, sin, pi, Matrix, lambdify 
 from scipy.optimize import least_squares
+import csv
 
-offsets = np.array([np.radians(0), -np.pi/2, np.radians(0), np.radians(0), np.radians(0), np.radians(0)])
-lower_bounds = np.radians(np.array([-165, -90, -180, -160, -115, -175])) - offsets
-upper_bounds = np.radians(np.array([165, 90, 65, 160, 115, 175])) - offsets
-
+offsets = [0, 0, 0, 0, 0, 0]
+q1, q2, q3, q4, q5, q6 = symbols('q1 q2 q3 q4 q5 q6')
+lower_bounds = np.array([-165, -90, -180, -160, -115, -175])
+upper_bounds = np.array([165, 90, 65, 160, 115, 175])
+DH = [
+    # [a, alpha, d, theta] for joint 1, then do for joint 2, 3, 4, 5, 6
+    [0, np.radians(0), 114, q1],
+    [0, np.radians(-90), 0, q2-pi/2],
+    [95, np.radians(0), 0, q3],
+    [10, np.radians(-90), 95, q4],
+    [0, np.radians(90), 0, q5],
+    [0, np.radians(-90), 58, q6]
+]
 
 def get_transformation_matrix(a, alpha, d, theta):
     M = Matrix([[cos(theta), -sin(theta), 0, a],
-                [sin(theta)*cos(alpha), cos(theta)*cos(alpha), -sin(alpha), -sin(alpha)*d],
-                [sin(theta)*sin(alpha), cos(theta)*sin(alpha), cos(alpha), cos(alpha)*d],
+                [sin(theta)*np.cos(alpha), cos(theta)*np.cos(alpha), -np.sin(alpha), -np.sin(alpha)*d],
+                [sin(theta)*np.sin(alpha), cos(theta)*np.sin(alpha), np.cos(alpha), np.cos(alpha)*d],
                 [0, 0, 0, 1]])
     return M
 
+T = get_transformation_matrix(DH[0][0], DH[0][1], DH[0][2], DH[0][3])
+for i in range(1, len(DH)):
+    T = T @ get_transformation_matrix(DH[i][0], DH[i][1], DH[i][2], DH[i][3])
+
+q_sym = symbols('q1:7')
+
+q_sym = symbols('q1:7')
+def symbolic_forward_kinematics(q_values):
+    subs_dict = {q:offset + angle for q, offset, angle in zip([q1, q2, q3, q4, q5, q6], offsets, q_values)}
+    T_symbolic = T.subs(subs_dict)
+    return T_symbolic
+
+forward_kinematics_func = lambdify(q_sym, symbolic_forward_kinematics(q_sym), "numpy")
 
 # Converts the transformation matrix into a pose vector: [X, Y, Z, roll, pitch, yaw]
 def transf_to_pose(t_matrix):
     # Position (fill in indices based on your matrix structure)
-    X, Y, Z = t_matrix[0,3], t_matrix[1,3], t_matrix[2,3]
+    X, Y, Z = t_matrix[0][3], t_matrix[1][3], t_matrix[2][3]
 
     # Breakdown the rotation matrix into axes
     R = t_matrix[0:3, 0:3]
     roll  = np.arctan2(R[2,1], R[2,2])
     pitch = np.arctan2(-R[2,0], np.sqrt(R[0,0]**2 + R[1,0]**2))
-    yaw   = np.arctan2(R[1,0], R[0,0])
+    yaw   = np.arctan2(R[1,0], R[0,0]) # look into this
 
-    return np.array([X, Y, Z, roll, pitch, yaw], dtype=float)
+    return X, Y, Z, roll, pitch, yaw
 
-def wrap_to_pi(a):
-    return (a + np.pi) % (2*np.pi) - np.pi
 
 # Checks the current joint angles against the target pose and returns their delta
 def target_pose_error(q, x_target, y_target, z_target, rx_d, ry_d, rz_d):
-   q_full = q + offsets
-
-   current_fk = forward_kinematics_func(*q_full)
+   current_fk = forward_kinematics_func(*q)
 
    # Convert translation matrix to pose vector
    current_pose = transf_to_pose(current_fk)
-   target_pose = np.array([x_target, y_target, z_target, rx_d, ry_d, rz_d])
+   target_pose = [x_target, y_target, z_target, rx_d, ry_d, rz_d]
 
-   error = target_pose - current_pose
-   error[3:] = wrap_to_pi(error[3:])
+   error = np.array(current_pose) - np.array(target_pose)
    return error
 
 
 # Looks for a solution to inverse kinematics for a target pose and returns a transformation matrix
-def inverse_kinematics(x_target, y_target, z_target, rx_d, ry_d, rz_d, q_init, max_iter=1000, tolerance=1e-5):
-    target_pose = (x_target, y_target, z_target, rx_d, ry_d, rz_d)
+def inverse_kinematics(target_pose, init_pose, max_iter=1000, tolerance=1e-5, bounds=[lower_bounds,upper_bounds]):
 
     result = least_squares(target_pose_error, 
-                            q_init, 
+                            init_pose, 
                             args=target_pose, 
                             method='trf', 
                             max_nfev=max_iter, 
                             ftol=tolerance, 
-                            bounds=(lower_bounds, upper_bounds))
+                            bounds=bounds)
 
     if result.success:
         print(f"Inverse kinematics converged after {result.nfev} function evaluations.")
@@ -66,42 +83,22 @@ def inverse_kinematics(x_target, y_target, z_target, rx_d, ry_d, rz_d, q_init, m
         return None
 
 
-if __name__ == "__main__":
-    q1, q2, q3, q4, q5, q6 = symbols('q1 q2 q3 q4 q5 q6')
-    DH = [
-        # [a, alpha, d, theta] for joint 1, then do for joint 2, 3, 4, 5, 6
-        [0, np.radians(0), 114, q1],
-        [0, -np.pi/2, 0, q2],
-        [95, np.radians(0), 0, q3],
-        [10, -np.pi/2, 95, q4],
-        [0, np.pi/2, 0, q5],
-        [0, -np.pi/2, 63.55, q6]
-    ]
 
-    T_01 = get_transformation_matrix(DH[0][0], DH[0][1], DH[0][2], DH[0][3])
-    T_12 = get_transformation_matrix(DH[1][0], DH[1][1], DH[1][2], DH[1][3])
-    T_23 = get_transformation_matrix(DH[2][0], DH[2][1], DH[2][2], DH[2][3])
-    T_34 = get_transformation_matrix(DH[3][0], DH[3][1], DH[3][2], DH[3][3])
-    T_45 = get_transformation_matrix(DH[4][0], DH[4][1], DH[4][2], DH[4][3])
-    T_56 = get_transformation_matrix(DH[5][0], DH[5][1], DH[5][2], DH[5][3])
-    T_sym = T_01 * T_12 * T_23 * T_34 * T_45 * T_56
+x_target = 218.6
+y_target = 91.3
+z_target = 144.3
+rx_d = np.radians(-137.69)  # Roll angle (in radians)
+ry_d = np.radians(-68.97)  # Pitch angle (in radians)
+rz_d = np.radians(-28.38)  # Yaw angle (in radians)
 
-    forward_kinematics_func = lambdify((q1, q2, q3, q4, q5, q6), T_sym, "numpy")
+target_pose = [x_target, y_target, z_target, rx_d, ry_d, rz_d]
+init_pose = [2.1,65.83,-51.32,-5.53,57.3,149.94]
+joint_angles = inverse_kinematics(target_pose, init_pose)
+        
+# output the joint angles. You may save the output to a csv file
+if joint_angles is not None:
+    print("Joint Angles (Degrees):", joint_angles)
+    print("True Angles (Degrees):", [24.69,60.02,-51.32,-51.32,10.89,-112.32])
+else:
+    print("Joint Angles (Degrees): None")
 
-    # Example (Replace these values with collected coords from Lab 2)
-
-    x_target = 218.6
-    y_target = 91.3
-    z_target = 144.3
-    rx_d = np.radians(-137.69)  # Roll angle (in radians)
-    ry_d = np.radians(-68.97)  # Pitch angle (in radians)
-    rz_d = np.radians(-28.38)  # Yaw angle (in radians)
-    init_pose = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
-    joint_angles = inverse_kinematics(x_target, y_target, z_target, rx_d, ry_d, rz_d, init_pose)
-            
-    # output the joint angles. You may save the output to a csv file
-    if joint_angles is not None:
-        print("Joint Angles (Degrees):", np.degrees(joint_angles))
-        print("True Angles (Degrees):", [24.69,60.02,-51.32,-51.32,10.89,-112.32])
-    else:
-        print("Joint Angles (Degrees): None")
